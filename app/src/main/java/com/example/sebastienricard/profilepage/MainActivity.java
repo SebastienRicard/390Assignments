166278340db5548525c6725d825804c0a774541f
Sebastien Ricard <Dyno_mite3@hotmail.com>
2016-09-25 12:05:48 -0400

    Second Commit. Added Coordinator Layout, implemented Profile Button

--- app/src/main/java/com/example/sebastienricard/profilepage/MainActivity.java
+++ app/src/main/java/com/example/sebastienricard/profilepage/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         else
             ProfileButton.setText(name);
 
+        ProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfilePage.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
