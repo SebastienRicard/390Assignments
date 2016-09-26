efa9f950d14efbb5edb365514576b9538e6a1f7d
Sebastien Ricard <Dyno_mite3@hotmail.com>
2016-09-25 23:45:36 -0400

    Third Commit. Edit button tool bar, Visibility

--- app/src/main/java/com/example/sebastienricard/profilepage/ProfilePage.java
+++ app/src/main/java/com/example/sebastienricard/profilepage/ProfilePage.java
@@ -2,15 +2,20 @@ package com.example.sebastienricard.profilepage;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ProfilePage extends AppCompatActivity {
 
     protected EditText NameEditText;
+    protected TextView BMIText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class ProfilePage extends AppCompatActivity {
         setContentView(R.layout.activity_profile_page);
 
         NameEditText = (EditText) findViewById(R.id.NameEditText);
+        BMIText = (TextView) findViewById(R.id.BMIView);
         final Button SaveButton = (Button) findViewById(R.id.SaveButton);
         SaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +35,26 @@ public class ProfilePage extends AppCompatActivity {
                 editor.commit();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.action_edit:
+                BMIText.setVisibility(View.GONE);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
